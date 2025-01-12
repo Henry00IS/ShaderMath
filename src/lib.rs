3f64cbe2cb755244b93747b88cae8baa3703c339
Henry de Jongh <henry00@live.nl>
2025-01-12 18:09:59 +0100

    Fixed compilation errors.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,5 @@
 mod math;
-use math::Float2;
-
 mod tests;
 
-fn main() {
-    let mut a = Float2::new(20.0, 1.0);
-    let b = a;
-    a += b;
-    println!("{}", a);
-}
+pub use math::Float2;
+pub use math::Float3;
